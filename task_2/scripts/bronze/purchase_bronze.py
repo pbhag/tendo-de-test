638297a49985e7ac94a10c9686574b5c9f0b8649
Prashant Bhagavatula <prashant.bhagavatula@saks.com>
2024-06-30 12:18:46 -0700

    creating rest of bronze loads

---
+++ task_2/scripts/bronze/purchase_bronze.py
@@ -0,0 +1,31 @@
+import sys 
+import os
+from pyspark.sql import SparkSession
+from task_2.utils.util import clean_column_names, add_metadata, create_table_if_not_exists, log_error
+
+def main():
+    script_name = os.path.basename(__file__).replace(".py", "")  # Get the script name without extension
+    spark = SparkSession.builder.appName("PurchaseBronzeLayer").getOrCreate()
+
+    s3_path = "s3://tendo-de-test/purchase.csv" # TODO: look for filename patterns for future loads
+    table_name = "tendo.bronze.purchase"
+    ddl_path = "ddl/create_bronze_tables.sql"
+
+    try:
+        create_table_if_not_exists(spark, table_name, ddl_path)
+
+        df = spark.read.option("header", "true").csv(s3_path)
+        cleaned_df = clean_column_names(df)
+        final_df = add_metadata(cleaned_df, s3_path)
+
+        final_df.write.format("delta").mode("append").saveAsTable(table_name)
+        print(f"Data from {s3_path} ingested to Bronze successfully.")
+    except Exception as e:
+        error_message = f"Error ingesting data from {s3_path} to Bronze: {e}"
+        print(error_message)
+        log_error(error_message, script_name=script_name, log_dir="/dbfs/logs/")
+    finally:
+        spark.stop()
+
+if __name__ == "__main__":
+    main()

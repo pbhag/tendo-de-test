f171a33025fb0ad997a27ac55ffdd7802390ee94
pbhag <pbhag>
2024-07-01 21:23:21 +0000

    creating notebooks

--- task_2/scripts/bronze/fertilizer_bronze.py
+++ task_2/scripts/bronze/fertilizer_bronze.py
@@ -1,3 +1,4 @@
+# Databricks notebook source
 from task_2.utils.load_utils import load_raw_data
 
 
@@ -9,5 +10,6 @@ username = spark.sql("SELECT regexp_replace(current_user(), '[^a-zA-Z0-9]', '_')
 checkpoint_path = f"/tmp/{username}/_checkpoint/etl_quickstart"
 
 load_raw_data(s3_directory, filename, table_name, ddl_path, checkpoint_path)
+
 
 

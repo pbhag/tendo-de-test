694181ffa33d2a92e89feda99b98e375ac87bf27
pbhag <pbhag>
2024-07-02 03:16:37 +0000

    updating autoloader

--- task_2/scripts/bronze/Untitled Notebook 2024-07-01 14_54_38.py
+++ task_2/scripts/bronze/Untitled Notebook 2024-07-01 14_54_38.py
@@ -1,15 +1,12 @@
 # Databricks notebook source
 from pyspark.sql.functions import col, current_timestamp
-from task_2.utils.load_utils import create_table_if_not_exists
 
 file_path = "s3://tendo-de-test/avocado" # TODO: look for filename patterns for future loads
-ddl_path = "ddl/create_bronze_tables.sql"
 
 username = spark.sql("SELECT regexp_replace(current_user(), '[^a-zA-Z0-9]', '_')").first()[0]
 table_name = f"tendo.bronze.avocado"
 checkpoint_path = f"/tmp/{username}/_checkpoint/etl_quickstart"
 
-create_table_if_not_exists(spark, table_name, ddl_path)
 
 df = (spark.readStream
   .format("cloudFiles")
@@ -31,3 +28,7 @@ df = (spark.readStream
   .option("checkpointLocation", checkpoint_path)
   .option("mergeSchema", "true")
   .toTable(table_name))
+
+# COMMAND ----------
+
+
--- task_2/scripts/bronze/purchase_bronze.py
+++ task_2/scripts/bronze/purchase_bronze.py
@@ -1,5 +1,6 @@
 # Databricks notebook source
-from task_2.utils.load_utils import load_raw_data
+from pyspark.sql.functions import col, current_timestamp
+from task_2.utils.load_utils import create_table_if_not_exists
 
 
 file_path = "s3://tendo-de-test/purchase" 
@@ -9,5 +10,28 @@ username = spark.sql("SELECT regexp_replace(current_user(), '[^a-zA-Z0-9]', '_')
 checkpoint_path = f"/tmp/{username}/_checkpoint/etl_quickstart"
 
 
-load_raw_data(file_path, table_name, ddl_path, checkpoint_path)
+create_table_if_not_exists(spark, table_name, ddl_path)
+
+df = (spark.readStream
+  .format("cloudFiles")
+  .option("cloudFiles.format", "csv")
+  .option("cloudFiles.schemaLocation", checkpoint_path)
+  .option("header", "true")
+  .option("inferSchema", "true")
+  .load(file_path)
+  .select("purchaseid",
+          "consumerid"
+          "graphed_date",
+          "avocado_bunch_id", 
+          "reporting_year",
+          col("QA process").alias("qa_process"),
+          "billing_provider_sku",
+          "picked_date",
+          "grocery_store_id",
+          "price_index",
+          col("_metadata.file_path").alias("source_file"), current_timestamp().alias("processing_time"))
+  .writeStream
+  .option("checkpointLocation", checkpoint_path)
+  .option("mergeSchema", "true")
+  .toTable(table_name))
 
